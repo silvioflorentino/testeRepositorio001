5db3c9b1e69f9850fb118a37cf7c5454ccd26961
silvioflorentino <professorsilvioflorentino@yahoo.com.br>
2023-02-07 21:23:59 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let ladoA = prompt("Digite o lado A");
+let ladoB = prompt("Digite o lado B");
+
+let calculo = ladoB * ladoA;
+
+document.getElementById("valor1").innerHTML = ladoA;
+document.getElementById("valor2").innerHTML = ladoB;
+document.getElementById("resultado").innerHTML = calculo;
